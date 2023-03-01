97256d943f223152148968f8f931c876cd72ef82
Nawaf <125497395+Nawaf-SWE@users.noreply.github.com>
2023-03-01 18:16:44 +0300

    added comments and test case

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
+# @param x: number
+# @returns x squared
 def square(x):
     return pow(x, 2)
 
 
 print("the square of 2 is", square(2))
 print("the square of 3 is", square(3))
+print("the square of 4 is", square(4))
